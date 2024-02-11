1246a3ec2971f86ba9fb53772f7bdf13577398f0
jayantgomase <jayantgomase3@gmail.com>
2024-02-11 21:22:12 +0530

    category dao added

---
+++ swapvista-backend/src/main/java/com/swapvista/dao/CategoryDao.java
@@ -0,0 +1,17 @@
+package com.swapvista.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.biddingsystem.entity.Category;
+
+@Repository
+public interface CategoryDao extends JpaRepository<Category, Integer> {
+
+	List<Category> findByStatusIn(List<String> status);
+
+	Long countByStatusIn(List<String> status);
+
+}
