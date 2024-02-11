9e03bdf04e09d65b45a4cbabb48b6179d8fae66c
Garima Chauhan <garimachauhan.2999@gmail.com>
2024-02-11 14:11:49 +0530

    adding maven project

---
+++ swapvista-backend/swapvista/src/main/java/com/swapvista/SwapvistaApplication.java
@@ -0,0 +1,16 @@
+package com.swapvista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapvistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwapvistaApplication.class, args);
+	}
+
+
+	
+
+}
