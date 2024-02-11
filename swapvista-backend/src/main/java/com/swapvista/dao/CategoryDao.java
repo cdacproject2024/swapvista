be82a916a6fe5460b4c0210ab383132381850f9b
jayantgomase <jayantgomase3@gmail.com>
2024-02-11 21:39:14 +0530

    update dao category

--- swapvista-backend/src/main/java/com/swapvista/dao/CategoryDao.java
+++ swapvista-backend/src/main/java/com/swapvista/dao/CategoryDao.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.biddingsystem.entity.Category;
+import com.swapvista.entity.Category;
 
 @Repository
 public interface CategoryDao extends JpaRepository<Category, Integer> {
