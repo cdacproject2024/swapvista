53d6646e0b01ae6aee852df0954a97689e92deb6
kshitijg12 <kshitijghormade2@gmail.com>
2024-02-12 22:33:47 +0530

    added CommonApiResponseDto and RegisterUserRequestDto

---
+++ swapvista-backend/src/main/java/com/swapvista/dto/RegisterUserRequestDto.java
@@ -0,0 +1,41 @@
+package com.swapvista.dto;
+
+import org.springframework.beans.BeanUtils;
+
+import com.swapvista.entity.User;
+
+import lombok.Data;
+
+@Data
+public class RegisterUserRequestDto {
+	
+	private String firstName;
+
+	private String lastName;
+
+	private String emailId;
+
+	private String password;
+
+	private String phoneNo;
+
+	private String role;
+	
+	private String street;
+
+	private String city;
+
+	private int pincode;
+	
+	private int sellerId;   
+	
+	// Converts RegisterUserRequestDto to User entity
+	public static User toUserEntity(RegisterUserRequestDto registerUserRequestDto) {
+		User user =new User();
+		
+    // Copies properties from RegisterUserRequestDto to User entity
+	BeanUtils.copyProperties(registerUserRequestDto, user);		
+		return user;
+	}
+	
+}
