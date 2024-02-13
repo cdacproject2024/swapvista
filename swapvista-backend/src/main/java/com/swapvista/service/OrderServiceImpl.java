522feb0a872596379a30ace97ce9b4592560be1a
kshitijg12 <kshitijghormade2@gmail.com>
2024-02-13 22:37:18 +0530

    Update OrderServiceImpl.java

--- swapvista-backend/src/main/java/com/swapvista/service/OrderServiceImpl.java
+++ swapvista-backend/src/main/java/com/swapvista/service/OrderServiceImpl.java
@@ -14,35 +14,54 @@ import com.swapvista.entity.User;
 public class OrderServiceImpl implements OrderService {
 
 	@Autowired
+<<<<<<< Updated upstream
 	//Injecting dependency of OrdersDao for data access
 	private OrdersDao orderDao;
 
 	//Adds orders to the system
+=======
+	private OrdersDao orderDao;
+
+>>>>>>> Stashed changes
 	@Override
 	public List<Orders> addOrder(List<Orders> orders) {
 		return this.orderDao.saveAll(orders);
 	}
+<<<<<<< Updated upstream
  
 	//Updates one or more existing orders in the system
+=======
+
+>>>>>>> Stashed changes
 	@Override
 	public List<Orders> updateOrders(List<Orders> orders) {
 		return this.orderDao.saveAll(orders);
 	}
 
+<<<<<<< Updated upstream
 	//Retrieves orders based on their order ID
+=======
+>>>>>>> Stashed changes
 	@Override
 	public List<Orders> getOrdersByOrderId(String orderId) {
 
 		return this.orderDao.findByOrderId(orderId);
 
 	}
+<<<<<<< Updated upstream
 	//Retrieves a single order based on its unique ID
+=======
+
+>>>>>>> Stashed changes
 	@Override
 	public Orders getOrderById(int orderId) {
 
 		Optional<Orders> optionalOrder = this.orderDao.findById(orderId);
 
+<<<<<<< Updated upstream
 		//Return the order if present, otherwise return null
+=======
+>>>>>>> Stashed changes
 		if (optionalOrder.isPresent()) {
 			return optionalOrder.get();
 		} else {
@@ -51,37 +70,56 @@ public class OrderServiceImpl implements OrderService {
 
 	}
 
+<<<<<<< Updated upstream
 	//Retrieves orders associated with a specific user
+=======
+>>>>>>> Stashed changes
 	@Override
 	public List<Orders> getOrdersByUser(User user) {
 		return orderDao.findByUser(user);
 	}
 
+<<<<<<< Updated upstream
 	//Updates a single order
+=======
+>>>>>>> Stashed changes
 	@Override
 	public Orders updateOrder(Orders order) {
 		return orderDao.save(order);
 	}
 
+<<<<<<< Updated upstream
 	//Retrieves orders based on order ID and a list of statuses
+=======
+>>>>>>> Stashed changes
 	@Override
 	public List<Orders> getOrdersByOrderIdAndStatusIn(String orderId, List<String> status) {
 		return this.orderDao.findByOrderIdAndStatusIn(orderId, status);
 	}
+<<<<<<< Updated upstream
 	
 	//Retrieves orders for a specific user and a list of statuses
+=======
+
+>>>>>>> Stashed changes
 	@Override
 	public List<Orders> getOrdersByUserAndStatusIn(User user, List<String> status) {
 		return this.orderDao.findByUserAndStatusIn(user, status);
 	}
 
+<<<<<<< Updated upstream
 	//Retrieves orders for a specific seller and a list of statuses
+=======
+>>>>>>> Stashed changes
 	@Override
 	public List<Orders> getOrdersBySellerAndStatusIn(User user, List<String> status) {
 		return this.orderDao.findAllOrdersBySellerAndStatusIn(user, status);
 	}
 
+<<<<<<< Updated upstream
 	//Retrieves all orders in the system
+=======
+>>>>>>> Stashed changes
 	@Override
 	public List<Orders> getAllOrders() {
 		return this.orderDao.findAll();
