69ce97d16830726192875c311c110306290496db
Garima Chauhan <garimachauhan.2999@gmail.com>
2024-02-14 15:44:23 +0530

    Creating config package

---
+++ swapvista-backend/src/main/java/com/swapvista/config/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.swapvista.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import com.swapvista.entity.User;
+import com.swapvista.service.UserService;
+import com.swapvista.utility.Constants.UserStatus;
+
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UserService userService;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+		User user = this.userService.getUserByEmailAndStatus(email, UserStatus.ACTIVE.value());
+
+		CustomUserDetails customUserDetails = new CustomUserDetails(user);
+
+		return customUserDetails;
+
+	}
+}
