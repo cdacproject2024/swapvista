2b0a14fdeaff8b2ecb00b77bd97937fb1e3637af
Garima Chauhan <garimachauhan.2999@gmail.com>
2024-02-14 12:01:18 +0530

    Creating new resource package

---
+++ swapvista-backend/src/main/java/com/swapvista/resource/UserResource.java
@@ -0,0 +1,11 @@
+package com.swapvista.resource;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.transaction.Transactional;
+
+@Component
+@Transactional
+public class UserResource {
+	
+}
